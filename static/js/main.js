039cdc78380c3f4e35cbdf3e6e06123fdb8e0dab
wezmiolem <ch.maciek@gmail.com>
2021-01-29 18:18:20 +0100

    adding boards

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
-accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+accordionButtons = document.querySelectorAll(".dropdown");
 
-accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener("click", event => {
-
+accordionButtons.forEach(accordionButton => {
+    accordionButton.addEventListener("click", event => {
+        const accordionItemHeader = accordionButton.parentElement;
         accordionItemHeader.classList.toggle("active");
         const accordionItemBody = accordionItemHeader.nextElementSibling;
         if (accordionItemHeader.classList.contains("active")) {
@@ -11,6 +11,31 @@ accordionItemHeaders.forEach(accordionItemHeader => {
         else {
             accordionItemBody.style.maxHeight = 0;
         }
-
     });
 });
+
+newBoardButton = document.querySelector(".new-board");
+
+newBoardButton.addEventListener("click", createBoard);
+
+function createBoard() {
+    let boardName = prompt("Board Name");
+    let newBoard = document.createElement("div");
+    newBoard.classList.add("accordion-item");
+    let newBoardHeader = document.createElement("div");
+    newBoardHeader.classList.add("accordion-item-header");
+    let boardNameHolder = document.createElement("span");
+    boardNameHolder.innerHTML = boardName;
+    boardNameHolder.setAttribute("contenteditable", "true");
+    newBoardHeader.appendChild(boardNameHolder);
+    let accordionButton = document.createElement("button");
+    accordionButton.classList.add("dropdown");
+    newBoardHeader.appendChild(accordionButton);
+    newBoard.appendChild(newBoardHeader);
+    let newBoardBody = document.createElement('div');
+    newBoardBody.classList.add("accordion-item-body");
+    newBoardBody.innerHTML = "test";
+    newBoard.appendChild(newBoardBody);
+    let boardsHolder = document.querySelector(".accordion");
+    boardsHolder.appendChild(newBoard);
+}
