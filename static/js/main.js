0bf92afdc5d88978056aa2942a7a21aeb1be6771
wezmiolem <ch.maciek@gmail.com>
2021-02-03 12:52:29 +0100

    adding boards with columns

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,4 @@
-accordionButtons = document.querySelectorAll(".dropdown");
-
-accordionButtons.forEach(accordionButton => {
-    accordionButton.addEventListener("click", event => {
-        const accordionItemHeader = accordionButton.parentElement;
-        accordionItemHeader.classList.toggle("active");
-        const accordionItemBody = accordionItemHeader.nextElementSibling;
-        if (accordionItemHeader.classList.contains("active")) {
-            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-        }
-        else {
-            accordionItemBody.style.maxHeight = 0;
-        }
-    });
-});
+const columnNames = ["New", "In Progress", "Testing", "Done"];
 
 newBoardButton = document.querySelector(".new-board");
 
@@ -27,15 +13,43 @@ function createBoard() {
     let boardNameHolder = document.createElement("span");
     boardNameHolder.innerHTML = boardName;
     boardNameHolder.setAttribute("contenteditable", "true");
+    boardNameHolder.setAttribute("placeholder", "Board Name")
     newBoardHeader.appendChild(boardNameHolder);
     let accordionButton = document.createElement("button");
     accordionButton.classList.add("dropdown");
+    enableAccordion(accordionButton);
     newBoardHeader.appendChild(accordionButton);
     newBoard.appendChild(newBoardHeader);
-    let newBoardBody = document.createElement('div');
+    let newBoardBody = document.createElement("div");
     newBoardBody.classList.add("accordion-item-body");
-    newBoardBody.innerHTML = "test";
+    let newBoardBodyContent = document.createElement("div");
+    newBoardBodyContent.classList.add("accordion-item-body-content");
+    columnNames.forEach(function (columnName) {
+        let column = document.createElement("div");
+        column.classList.add("column");
+        let columnTitle = document.createElement("span")
+        columnTitle.innerHTML = columnName
+        columnTitle.setAttribute("contenteditable", "true")
+        columnTitle.setAttribute("placeholder", columnName)
+        column.appendChild(columnTitle)
+        newBoardBodyContent.appendChild(column)
+    })
+    newBoardBody.appendChild(newBoardBodyContent);
     newBoard.appendChild(newBoardBody);
     let boardsHolder = document.querySelector(".accordion");
     boardsHolder.appendChild(newBoard);
+}
+
+function enableAccordion(accordionButton) {
+    accordionButton.addEventListener("click", () => {
+        const accordionItemHeader = accordionButton.parentElement;
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
+    })
 }
